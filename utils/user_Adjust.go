a3fdd531b038da6b4e4ad6a21369fd86068d420d
sobhanhs <sobhhanhsa1@gmail.com>
2023-06-15 15:10:17 +0330

    correction

--- utils/user_Adjust.go
+++ utils/user_Adjust.go
@@ -9,22 +9,4 @@ func UserAdjust(userstruct interface{}) (models.User, bool) {
 	user, ok := userstruct.(models.User)
 
 	return user, ok
-
-	// input := fmt.Sprintf(`%+v`, userstruct)
-
-	// // input = strings.ReplaceAll(input, "Model:", "")
-
-	// fmt.Println(input)
-
-	// var user models.User
-
-	// err := json.Unmarshal([]byte(input), &user)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return user, false
-	// }
-
-	// return user, true
-
 }
